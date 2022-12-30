49853797f6a79280dfd4a44169a6401190fe307f
Süleyman Şen <83352965+suleymansennn@users.noreply.github.com>
2022-12-30 14:20:16 +0300

    Update flo_cltv_prediction.py

--- flo_cltv_prediction.py
+++ flo_cltv_prediction.py
@@ -37,7 +37,7 @@ Customer LifeTime Value Prediction
 """
 
 """
-İş Problemi:
+Business Problem:
 FLO would like to set a roadmap for sales and marketing activities. In order for the company to make a medium-long-term plan,
 it is necessary to estimate the potential value that existing customers will provide to the company in the future.
 """
